e75c85567fd76786a50ec694c7eefde2753c3c0a
blastcraft <git@blastcraft.net>
2015-03-03 19:14:31 +0300

    Form elements added

---
+++ ExplProducts/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace ExplProducts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.labelC = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxO = new System.Windows.Forms.TextBox();
+            this.labelO = new System.Windows.Forms.Label();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.labelN = new System.Windows.Forms.Label();
+            this.textBoxH = new System.Windows.Forms.TextBox();
+            this.labelH = new System.Windows.Forms.Label();
+            this.textBoxCl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxK = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCa = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxNa = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxAl = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonLoadData = new System.Windows.Forms.Button();
+            this.buttonSaveData = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(26, 19);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(69, 20);
+            this.textBoxC.TabIndex = 0;
+            this.textBoxC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxC_KeyPress);
+            // 
+            // labelC
+            // 
+            this.labelC.AutoSize = true;
+            this.labelC.Location = new System.Drawing.Point(6, 22);
+            this.labelC.Name = "labelC";
+            this.labelC.Size = new System.Drawing.Size(14, 13);
+            this.labelC.TabIndex = 1;
+            this.labelC.Text = "C";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxAl);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBoxNa);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxCa);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxK);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxF);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxCl);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxO);
+            this.groupBox1.Controls.Add(this.labelO);
+            this.groupBox1.Controls.Add(this.textBoxN);
+            this.groupBox1.Controls.Add(this.labelN);
+            this.groupBox1.Controls.Add(this.textBoxH);
+            this.groupBox1.Controls.Add(this.labelH);
+            this.groupBox1.Controls.Add(this.textBoxC);
+            this.groupBox1.Controls.Add(this.labelC);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(107, 280);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Брутто-формула";
+            // 
+            // textBoxO
+            // 
+            this.textBoxO.Location = new System.Drawing.Point(26, 97);
+            this.textBoxO.Name = "textBoxO";
+            this.textBoxO.Size = new System.Drawing.Size(69, 20);
+            this.textBoxO.TabIndex = 3;
+            this.textBoxO.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxO_KeyPress);
+            // 
+            // labelO
+            // 
+            this.labelO.AutoSize = true;
+            this.labelO.Location = new System.Drawing.Point(6, 100);
+            this.labelO.Name = "labelO";
+            this.labelO.Size = new System.Drawing.Size(15, 13);
+            this.labelO.TabIndex = 7;
+            this.labelO.Text = "O";
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(26, 71);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(69, 20);
+            this.textBoxN.TabIndex = 2;
+            this.textBoxN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxN_KeyPress);
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(6, 74);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(15, 13);
+            this.labelN.TabIndex = 5;
+            this.labelN.Text = "N";
+            // 
+            // textBoxH
+            // 
+            this.textBoxH.Location = new System.Drawing.Point(26, 45);
+            this.textBoxH.Name = "textBoxH";
+            this.textBoxH.Size = new System.Drawing.Size(69, 20);
+            this.textBoxH.TabIndex = 1;
+            this.textBoxH.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxH_KeyPress);
+            // 
+            // labelH
+            // 
+            this.labelH.AutoSize = true;
+            this.labelH.Location = new System.Drawing.Point(6, 48);
+            this.labelH.Name = "labelH";
+            this.labelH.Size = new System.Drawing.Size(15, 13);
+            this.labelH.TabIndex = 3;
+            this.labelH.Text = "H";
+            // 
+            // textBoxCl
+            // 
+            this.textBoxCl.Location = new System.Drawing.Point(26, 123);
+            this.textBoxCl.Name = "textBoxCl";
+            this.textBoxCl.Size = new System.Drawing.Size(69, 20);
+            this.textBoxCl.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Cl";
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(26, 149);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.Size = new System.Drawing.Size(69, 20);
+            this.textBoxF.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "F";
+            // 
+            // textBoxK
+            // 
+            this.textBoxK.Location = new System.Drawing.Point(26, 175);
+            this.textBoxK.Name = "textBoxK";
+            this.textBoxK.Size = new System.Drawing.Size(69, 20);
+            this.textBoxK.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "K";
+            // 
+            // textBoxCa
+            // 
+            this.textBoxCa.Location = new System.Drawing.Point(26, 201);
+            this.textBoxCa.Name = "textBoxCa";
+            this.textBoxCa.Size = new System.Drawing.Size(69, 20);
+            this.textBoxCa.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Ca";
+            // 
+            // textBoxNa
+            // 
+            this.textBoxNa.Location = new System.Drawing.Point(26, 227);
+            this.textBoxNa.Name = "textBoxNa";
+            this.textBoxNa.Size = new System.Drawing.Size(69, 20);
+            this.textBoxNa.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Na";
+            // 
+            // textBoxAl
+            // 
+            this.textBoxAl.Location = new System.Drawing.Point(26, 253);
+            this.textBoxAl.Name = "textBoxAl";
+            this.textBoxAl.Size = new System.Drawing.Size(69, 20);
+            this.textBoxAl.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Al";
+            // 
+            // buttonLoadData
+            // 
+            this.buttonLoadData.Location = new System.Drawing.Point(467, 12);
+            this.buttonLoadData.Name = "buttonLoadData";
+            this.buttonLoadData.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadData.TabIndex = 10;
+            this.buttonLoadData.Text = "Load Data";
+            this.buttonLoadData.UseVisualStyleBackColor = true;
+            // 
+            // buttonSaveData
+            // 
+            this.buttonSaveData.Location = new System.Drawing.Point(467, 41);
+            this.buttonSaveData.Name = "buttonSaveData";
+            this.buttonSaveData.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveData.TabIndex = 11;
+            this.buttonSaveData.Text = "Save Data";
+            this.buttonSaveData.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 306);
+            this.Controls.Add(this.buttonSaveData);
+            this.Controls.Add(this.buttonLoadData);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(570, 345);
+            this.MinimumSize = new System.Drawing.Size(570, 345);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Products Of Explosion";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Label labelC;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxO;
+        private System.Windows.Forms.Label labelO;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.TextBox textBoxH;
+        private System.Windows.Forms.Label labelH;
+        private System.Windows.Forms.TextBox textBoxAl;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxNa;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxCa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxK;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonLoadData;
+        private System.Windows.Forms.Button buttonSaveData;
+    }
+}
+
---
+++ ExplProducts/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExplProducts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == 46 && textBoxC.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxH_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == 46 && textBoxH.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxN_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == 46 && textBoxN.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxO_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == 46 && textBoxO.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
